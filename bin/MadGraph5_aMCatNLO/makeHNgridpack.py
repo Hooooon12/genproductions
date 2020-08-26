809123331ab2c249b316064a9f0a49b280df086d
Hooooon12 <theking461@snu.ac.kr>
2020-08-26 16:52:26 +0900

    make gridpacks

---
+++ bin/MadGraph5_aMCatNLO/makeHNgridpack.py
@@ -0,0 +1,9 @@
+import os
+
+#masses = ["85","90","95","100","125","150","200","250","300","400","500","600","700","800","900","1000","1100","1200","1300","1500","1700","2000"]
+masses = ["85","90","95","100","125","150","200","250","300","400","500"]
+channels = ["MuMu"]
+
+for channel in channels:
+  for mass in masses:
+    os.system("./MakeGridpack.sh "+"DYTypeI_NLO_{0}_M{1}".format(channel, mass))
