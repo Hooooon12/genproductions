f71bd64d7ac2317973f53ee3d8cd7e6e15ac3de3
Hooooon12 <theking461@snu.ac.kr>
2020-09-11 13:23:49 +0900

    cards for newproduction

--- bin/MadGraph5_aMCatNLO/MakeGridpack.sh
+++ bin/MadGraph5_aMCatNLO/MakeGridpack.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 PROCESSNAME=$1
-CARDPATH=/data2/Users/jihkim/genproductions/bin/MadGraph5_aMCatNLO/cards/production/13TeV/HeavyNeutrino_Dilepton_NLO/DYTypeI_NLO/${1}
+#CARDPATH=/data2/Users/jihkim/genproductions/bin/MadGraph5_aMCatNLO/cards/production/13TeV/HeavyNeutrino_Dilepton_NLO/DYTypeI_NLO/${1}
+#CARDPATH=/data2/Users/jihkim/genproductions/bin/MadGraph5_aMCatNLO/cards/HeavyNeutrino_Dilepton_NLO/DYTypeI_NLO/${1}
+CARDPATH=/data2/Users/jihkim/genproductions/bin/MadGraph5_aMCatNLO/cards/HeavyNeutrino_Dilepton_NLO/VBFTypeI_NLO/${1}
 
 echo "PROCESSNAME=$PROCESSNAME"
 echo "CARDPATH=$CARDPATH"
@@ -14,7 +16,9 @@ fi
 
 SCRIPT=MG_MakeGridpack_${PROCESSNAME}.sh
 echo "#!/bin/bash" > $SCRIPT
-echo time env -i 'HOME=$HOME' bash -l -c \"source /cvmfs/cms.cern.ch/cmsset_default.sh\; ./gridpack_generation.sh $PROCESSNAME cards/production/13TeV/HeavyNeutrino_Dilepton_NLO/DYTypeI_NLO/$PROCESSNAME\" >>$SCRIPT
+#echo time env -i 'HOME=$HOME' bash -l -c \"source /cvmfs/cms.cern.ch/cmsset_default.sh\; ./gridpack_generation.sh $PROCESSNAME cards/production/13TeV/HeavyNeutrino_Dilepton_NLO/DYTypeI_NLO/$PROCESSNAME\" >>$SCRIPT
+#echo time env -i 'HOME=$HOME' bash -l -c \"source /cvmfs/cms.cern.ch/cmsset_default.sh\; ./gridpack_generation.sh $PROCESSNAME cards/HeavyNeutrino_Dilepton_NLO/DYTypeI_NLO/$PROCESSNAME\" >>$SCRIPT
+echo time env -i 'HOME=$HOME' bash -l -c \"source /cvmfs/cms.cern.ch/cmsset_default.sh\; ./gridpack_generation.sh $PROCESSNAME cards/HeavyNeutrino_Dilepton_NLO/VBFTypeI_NLO/$PROCESSNAME\" >>$SCRIPT
 chmod +x $SCRIPT
 
 nohup ./MG_MakeGridpack_${PROCESSNAME}.sh &
--- bin/MadGraph5_aMCatNLO/makeHNgridpack.py
+++ bin/MadGraph5_aMCatNLO/makeHNgridpack.py
@@ -1,9 +1,10 @@
 import os
 
-#masses = ["85","90","95","100","125","150","200","250","300","400","500"]
-masses = ["600","700","800","900","1000","1100","1200","1300","1500","1700","2000"]
-channels = ["EE"]
+#masses = ["85","90","95","100","125","150","200","250","400","500"]
+masses = ["500","600","700","800","900","1000","1100","1200","1300","1500","1700","2000"]
+channels = ["SF"]
 
 for channel in channels:
   for mass in masses:
-    os.system("./MakeGridpack.sh "+"DYTypeI_NLO_{0}_M{1}".format(channel, mass))
+    #os.system("./MakeGridpack.sh "+"DYTypeI_NLO_{0}_M{1}".format(channel, mass))
+    os.system("./MakeGridpack.sh "+"VBFTypeI_NLO_{0}_M{1}".format(channel, mass))
