28d206697f2079202d81cf6fd26b85a4c3e56d1b
Hooooon12 <theking461@snu.ac.kr>
2021-10-29 22:18:36 +0900

    slight refactor

--- bin/MadGraph5_aMCatNLO/MakeGridpack.sh
+++ bin/MadGraph5_aMCatNLO/MakeGridpack.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 PROCESSNAME=$1
+CHANNEL=$2
+MASS=$3
+FULLNAME=${1}TypeI_NLO_${2}_M${3}
 #CARDPATH=/data2/Users/jihkim/genproductions/bin/MadGraph5_aMCatNLO/cards/production/13TeV/HeavyNeutrino_Dilepton_NLO/DYTypeI_NLO/${1}
-CARDPATH=/data2/Users/jihkim/genproductions/bin/MadGraph5_aMCatNLO/cards/HeavyNeutrino_Dilepton_NLO/DYTypeI_NLO/${1}
+CARDPATH=/data2/Users/jihkim/genproductions/bin/MadGraph5_aMCatNLO/cards/HeavyNeutrino_Dilepton_NLO/${1}TypeI_NLO/${FULLNAME}
 #CARDPATH=/data2/Users/jihkim/genproductions/bin/MadGraph5_aMCatNLO/cards/HeavyNeutrino_Dilepton_NLO/VBFTypeI_NLO/${1} #JH : XXX change here
 
-echo "PROCESSNAME=$PROCESSNAME"
+echo "PROCESSNAME=$FULLNAME"
 echo "CARDPATH=$CARDPATH"
 
 if [ ! -e $CARDPATH ]
@@ -14,11 +17,11 @@ then
     exit 1
 fi
 
-SCRIPT=MG_MakeGridpack_${PROCESSNAME}.sh
+SCRIPT=MG_MakeGridpack_${FULLNAME}.sh
 echo "#!/bin/bash" > $SCRIPT
 #echo time env -i 'HOME=$HOME' bash -l -c \"source /cvmfs/cms.cern.ch/cmsset_default.sh\; ./gridpack_generation.sh $PROCESSNAME cards/production/13TeV/HeavyNeutrino_Dilepton_NLO/DYTypeI_NLO/$PROCESSNAME\" >>$SCRIPT
-echo time env -i 'HOME=$HOME' bash -l -c \"source /cvmfs/cms.cern.ch/cmsset_default.sh\; ./gridpack_generation.sh $PROCESSNAME cards/HeavyNeutrino_Dilepton_NLO/DYTypeI_NLO/$PROCESSNAME\" >>$SCRIPT
+echo time env -i 'HOME=$HOME' bash -l -c \"source /cvmfs/cms.cern.ch/cmsset_default.sh\; ./gridpack_generation.sh $FULLNAME cards/HeavyNeutrino_Dilepton_NLO/${1}TypeI_NLO/$FULLNAME\" >>$SCRIPT
 #echo time env -i 'HOME=$HOME' bash -l -c \"source /cvmfs/cms.cern.ch/cmsset_default.sh\; ./gridpack_generation.sh $PROCESSNAME cards/HeavyNeutrino_Dilepton_NLO/VBFTypeI_NLO/$PROCESSNAME\" >>$SCRIPT #JH XXX : change here
 chmod +x $SCRIPT
 
-nohup ./MG_MakeGridpack_${PROCESSNAME}.sh &
+nohup ./MG_MakeGridpack_${FULLNAME}.sh &
--- bin/MadGraph5_aMCatNLO/makeHNgridpack.py
+++ bin/MadGraph5_aMCatNLO/makeHNgridpack.py
@@ -1,12 +1,20 @@
 import os
 
 #masses = ["85","90","95","100","125","150","200","250","300","400","600","700","800","900","1100","1200","1300","1700","2000"]
-#masses = ["600","700","800","900","1000","1100","1200","1300","1500","1700","2000"]
-masses = ["300"]
-#channels = ["SF"]
-channels = ["DF"]
+#masses = ["600","1000","1200","750","2000","3000","5000"]
+#masses = ["10000","15000","20000"]
+#masses = ["750", "5000"]
+masses = ["15000"]
+channels = ["SF_top"]
+#channels = ["SF_V0p1"]
+#channels = ["DF"]
+#channels = ["SF","DF","MuMu"]
+#channels = ["SF","DF"]
+#channels = ["MuMu"]
+#processes = ["DY","VBF","SSWW"]
+processes = ["SSWW"]
 
-for channel in channels:
-  for mass in masses:
-    os.system("./MakeGridpack.sh "+"DYTypeI_NLO_{0}_M{1}".format(channel, mass))
-    #os.system("./MakeGridpack.sh "+"VBFTypeI_NLO_{0}_M{1}".format(channel, mass))
+for process in processes:
+  for channel in channels:
+    for mass in masses:
+      os.system("./MakeGridpack.sh "+process+" "+channel+" "+mass)
